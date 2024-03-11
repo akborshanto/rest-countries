a7211757a7ee877c082a9a4f9f0a751d187b01b1
akbor shanto <akborshanto2002@gmail.com>
2024-03-11 00:34:28 -0700

    flag add

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,25 +1,69 @@
 import React, { useEffect, useState } from 'react'
 import Country from './assets/countries/Country'
-
+import './assets/countru-name.css'
 const Countries = () => {
 const [countries,setCountries]=useState([])
+/* visited -countrye */
+const [visitedCountries,setVisitedCountries]=useState([])
+// const handleVisistedCountries=(countryu)=>{
+//     console.log(countryu)
+// }
+/* visitFlaga */
+const [visitFlag,setVisitFlag]=useState([])
+const hangleFlag=(flag)=>{
+ // console.log(flag)
+const newFlag=[...visitFlag,flag];
+setVisitFlag(newFlag)
 
+
+
+}
+
+const visitedHandle=(visit)=>{
+const newVisit=[...visitedCountries,visit]
+setVisitedCountries(newVisit)
+  //setVisitedCountries(visit)
+
+}
+
+/* useEffects */
 useEffect(()=>{
 fetch('https://restcountries.com/v3.1/all')
 .then(res=> res.json())
 .then(data=> setCountries(data))
-
-
 },[])
 
   return (
-    <div>
+    <div >
+
+
+    
+    <ul>
+    {
+      visitedCountries.map(li => <li>{li}</li>)
+    }
+    </ul>
+{/* flag */}
 {
-  countries.map(countrie=><Country item={countrie} key={Math.random()}></Country>)
+
+visitFlag.map(img => <img src={visitFlag} className='img' key={Math.random()}></img>)
+
 }
 
 
-    <h1>{countries.length}</h1>
+    <div className='country-name'>
+    {
+      countries.map(countrie=><Country hangleFlag={hangleFlag}  item={countrie} key={Math.random()} visitedHandle={visitedHandle}></Country>)
+    }
+    
+    
+        <h1>{countries.length}</h1>
+        <h1>{visitedCountries}</h1>
+    
+    
+    </div>
+
+
     </div>
   )
 }
--- src/assets/countries/Country.jsx
+++ src/assets/countries/Country.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './countriy.css'
-const Country = ({item}) => {
-    console.log(item.flags.png)
+const Country = ({item,visitedHandle,hangleFlag}) => {
+    //console.log(hangleFlag)
+//console.log(handleVisited.target)
+//console.log(visitedHandle)
+const [items,setItem]=useState(false)
+
+const handle=()=>{
+setItem(!items)
+
+
+}
+
+    const {name,flags,population,cca3}=item
+    //console.log(item.cca3)
 
   return (
-    <div className='country'>
-    <h1>NAME:{item.name.common}</h1>
-<img src={item.flags.png} alt={item.flags.alt} />
+    <div className={`country ${items ? 'visited' : 'non-visited'}`}>
+    <h1 className={`${items && 'blue'}`}>NAME:{name.common}</h1>
+<img src={flags.png} alt={item.flags.alt} />
+<p style={{color:'blue'}}>population:{population}</p>
+<p>{cca3}</p>
+<button onClick={()=>hangleFlag(item.flags.svg)}>HANGLE FLAG</button>
+<button onClick={()=>visitedHandle(item.name.common)} >Mark Visited</button>
+<button onClick={handle} >{items ? "GOING":"VISITED"}</button>
+{
+    items ? "nadia aktyere":"akbor shanto"
+    
+}
     </div>
   )
 }
